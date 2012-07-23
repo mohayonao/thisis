c7ef26f9ace77eea6a2f4973bc6c5363fa3064d1
mohayonao <mohayonao@gmail.com>
2012-07-23 20:04:52 +0900

    update

--- public/libs/js/footer.js
+++ public/libs/js/footer.js
@@ -3,7 +3,7 @@ jQuery(function() {
                                      "bottom":"10px", "left":"10px"});
 
     var $span = $("<span>").text(document.title + " / ");
-    var $a    = $("<a>").attr("target", "_blank").css("color", "blue").text("sourcecode");
+    var $a    = $("<a>").attr("target", "_blank").css("color", "blue").text("view this source");
 
     var src = "https://github.com/mohayonao/thisis/blob/master/public";
     src += location.pathname
